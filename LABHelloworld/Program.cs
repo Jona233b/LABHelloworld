f22d412654665daa808faa419802711f5ef036c5
jona233b <jona233b@edu.campusvejle.dk>
2022-01-17 09:58:13 +0100

    Add project files.

---
+++ LABHelloworld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LABHelloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
