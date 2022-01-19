bd83d0afcda1e130a14c112ae937c59f49cc6eca
Jona233b <jona233b@edu.campusvejle.dk>
2022-01-19 11:50:23 +0100

    før frokost

---
+++ Opgave_04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Opgave_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indtast fornavn.");
+            string userInput1 = Console.ReadLine();
+            Console.WriteLine("Indtast efternavn.");
+            string userInput2 = Console.ReadLine();
+            Console.WriteLine("Dit navn er");
+            Console.Write(userInput1);
+            Console.Write(" "); //så man få et mellenmrum
+            Console.Write(userInput2);
+            Console.WriteLine($" Velkommen { userInput1} { userInput2}!");
+
+        }
+    }
+}
