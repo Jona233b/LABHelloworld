49d81df76d2a816929530d22403ae4b45a378bc7
Jona233b <jona233b@edu.campusvejle.dk>
2022-01-21 11:25:07 +0100

    Console Calculator

--- Opgave_05/Program.cs
+++ Opgave_05/Program.cs
@@ -6,22 +6,42 @@ namespace Opgave_05
     {
         static void Main(string[] args)
         {
-            int tal1 = 0;
-            int tal2 = 0;
+            double tal1 = 0;
+            double tal2 = 0;
 
+            Console.WriteLine("Console Calculator Med C#");
 
+            Console.WriteLine("Indtast et tal, og tryk på Enter");
+            tal1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Velkommen til lommeregneren version 1.");
-            Console.WriteLine("Jeg er en simpel lommeregner, som udelukkende kan lægge to tel sammen.");
-            Console.WriteLine("Intast et tal");
+            Console.WriteLine("Indtast et andet nummer, og tryk på Enter");
+            tal2 = Convert.ToDouble(Console.ReadLine());
 
-            tal1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Vælg en mulighed fra følgende liste:");
+            Console.WriteLine("\t1 - Plus");
+            Console.WriteLine("\t2 - Minus");
+            Console.WriteLine("\t3 - Gange");
+            Console.WriteLine("\t4 - Divider");
+            Console.Write("Din mulighed?: ");
 
-            Console.WriteLine("Intast et nyt tal");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine($"Dit resultat: {tal1} + {tal2} = " + (tal1 + tal2));
+                    break;
+                case "2":
+                    Console.WriteLine($"Dit resultat: {tal1} - {tal2} = " + (tal1 - tal2));
+                    break;
+                case "3":
+                    Console.WriteLine($"Dit resultat: {tal1} * {tal2} = " + (tal1 * tal2));
+                    break;
+                case "4":
+                    Console.WriteLine($"Dit resultat: {tal1} / {tal2} = " + (tal1 / tal2));
+                    break;
+            }
 
-            tal2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine($"Resultat {tal1 + tal2}");
+            Console.Write("Tryk på en vilkårlig tast for at lukke Appen...");
+            Console.ReadKey();
 
         }
     }
