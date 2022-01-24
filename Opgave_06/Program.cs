7b2537ab9d608440ed9ff59c119fb10449f305f9
Jona233b <jona233b@edu.campusvejle.dk>
2022-01-24 14:04:17 +0100

    24-1-22

---
+++ Opgave_06/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Opgave_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 4;
+
+
+
+            Console.WriteLine(a == b); //false
+            Console.WriteLine(a > b); //false
+            Console.WriteLine(a <= b); //true
+            Console.WriteLine(a-b > 0); //false
+            Console.WriteLine(a+b > b); //true
+            Console.WriteLine(a > 1-b); //true
+        }
+    }
+}
---
+++ Opgave_07/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Opgave_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indset det beløb du ønsker at handle for:");
+
+            double price = double.Parse(Console.ReadLine());
+            double discount = 0;
+
+            if (price > 1000)
+            {
+                discount = price * 0.05;
+            }
+            Console.WriteLine($"Du får {discount} i rabat!");
+        }
+    }
+}
