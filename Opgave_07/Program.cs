b13a89d4ac0b8b550fe3cf321413801816edeeef
Jona233b <jona233b@edu.campusvejle.dk>
2022-01-31 14:21:04 +0100

    Boolsk algebra

--- Opgave_07/Program.cs
+++ Opgave_07/Program.cs
@@ -6,7 +6,7 @@ namespace Opgave_07
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Indset det beløb du ønsker at handle for:");
+            /*Console.WriteLine("Indset det beløb du ønsker at handle for:");
 
             double price = double.Parse(Console.ReadLine());
             double discount = 0;
@@ -16,6 +16,29 @@ namespace Opgave_07
                 discount = price * 0.05;
             }
             Console.WriteLine($"Du får {discount} i rabat!");
+            */
+
+            Console.WriteLine("Indtast to tal, og jeg skal fortælle hvilket er størst.");
+            Console.WriteLine("Indtast et heltal, og afslut med enter: ");
+
+            int inputOne = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Indtast et heltal, og afslut med enter: ");
+
+            int inputTwo = int.Parse(Console.ReadLine());
+
+            if (inputOne > inputTwo)
+            {
+                Console.WriteLine($"Det største tal er {inputOne}");
+            }
+            else if (inputTwo > inputOne)
+            {
+                Console.WriteLine($"Det største tal er {inputTwo}");
+            }
+            else
+            {
+                Console.WriteLine($"De to tal er ens.");
+            }
         }
     }
 }
